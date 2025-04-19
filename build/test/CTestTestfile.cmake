ca5ac6024832e0de3a7884045f1ed9f6dea094fc
Kyle Shores <kyle.shores44@gmail.com>
2025-04-19 00:02:39 -0500

    now removing build?

--- build/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kyle/Documents/cuda/test
-# Build directory: /home/kyle/Documents/cuda/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
