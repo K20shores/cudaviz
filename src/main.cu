4fa4b59b78407d7b6480a8ee26cb0914295bce29
Kyle Shores <kyle.shores44@gmail.com>
2025-05-12 21:53:29 -0500

    dot product

--- src/main.cu
+++ src/main.cu
@@ -5,6 +5,8 @@
 
 #include "check_error.hpp"
 
+constexpr int threadsPerBlock = 256;
+
 namespace kernels
 {
   __global__ void add(int *a, int *b, int *c, int N)
@@ -14,6 +16,39 @@ namespace kernels
     {
       c[tid] = a[tid] + b[tid];
       tid += blockDim.x * gridDim.x;
+    }
+  }
+
+  __global__ void dot(float *a, float *b, float* c, int N)
+  {
+    __shared__ float cache[threadsPerBlock];
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int cacheIdx = threadIdx.x;
+
+    float temp = 0;
+    while (tid < N)
+    {
+      temp += a[tid] + b[tid];
+      cacheIdx += blockIdx.x * gridDim.x;
+    }
+
+    cache[cacheIdx] += temp;
+
+    __syncthreads();
+
+    int i = blockDim.x / 2;
+    while (i != 0)
+    {
+      if (cacheIdx < i)
+      {
+        cache[cacheIdx] += cache[cacheIdx + i];
+      }
+      __syncthreads();
+      i /= 2;
+    }
+
+    if (cacheIdx == 0) {
+      c[blockIdx.x] = cache[0];
     }
   }
 }
@@ -83,7 +118,8 @@ void add()
 
   for (int i = 0; i < N; ++i)
   {
-    if (a[i] + b[i] != c[i]) {
+    if (a[i] + b[i] != c[i])
+    {
       std::cout << std::format("Error: {} != {} + {}\n", c[i], a[i], b[i]);
     }
   }
@@ -93,8 +129,45 @@ void add()
   CUDA_CHECK(cudaFree(dev_c));
 }
 
+#define imin(a, b) a<b?a:b
+void dot(){
+  constexpr int N = 2 << 20;
+  constexpr int blocks = imin(32, (N + threadsPerBlock - 1) / threadsPerBlock);
+
+  float *a, *b, *partial_c;
+  a = new float[N];
+  b = new float[N];
+  partial_c = new float[blocks];
+
+  for(int i = 0; i < N; ++i) {
+    a[i] = i;
+    b[i] = 1.0f / i;
+  }
+
+  float *dev_a, *dev_b, *dev_c;
+
+  CUDA_CHECK(cudaMalloc((void**)&dev_a, N * sizeof(float)));
+  CUDA_CHECK(cudaMalloc((void**)&dev_b, N * sizeof(float)));
+  CUDA_CHECK(cudaMalloc((void**)&dev_c, blocks * sizeof(float)));
+
+  CUDA_CHECK(cudaMemcpy(dev_a, a, N, cudaMemcpyHostToDevice));
+  CUDA_CHECK(cudaMemcpy(dev_b, b, N, cudaMemcpyHostToDevice));
+
+  kernels::dot<<<blocks, threadsPerBlock>>>(dev_a, dev_b, partial_c, N);
+
+  CUDA_CHECK(cudaMemcpy(partial_c, dev_c, blocks * sizeof(float), cudaMemcpyDeviceToHost));
+
+  float result = 0;
+  for(int i = 0; i < blocks; ++i) {
+    result += partial_c[i];
+  }
+
+  std::cout << std::format("Dot: {}\n", result);
+}
+
 int main()
 {
   device_data();
   add();
+  dot();
 }
